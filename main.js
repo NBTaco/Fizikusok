696d0b212ec7e5676c63275f4e3b9831ed466571
NBTaco <menandor119@gmail.com>
2025-01-05 13:54:34 +0100

    osszetett objektum bevezetese

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-const ter = "Fizika területe" //Létrehozunk egy változót a "Fizika területe"-nek
-const ido = "Időszak" //Létrehozunk egy változót az "Időszak"-nak
-const kepv = "Képviselők" //Létrehozunk egy változót a "Képviselők"-nek
-const ter1 = "Optika" //Létrehozunk egy változót az "Optika"-nak
-const ido1 = "XI. század" //Létrehozunk egy változót a "XI. század"-nak
-const kepv1 = "Alhazen" //Létrehozunk egy változót az "Alhazen"-nek
-const ter2 = "Asztronómia" //Létrehozunk egy változót az "Asztronómia"-nek
-const ido2 = "reneszánsz" //Létrehozunk egy változót a "reneszánsz"-nak
-const kepv2 = "Kepler" //Létrehozunk egy változót a "Kepler"-nek
-const kepv3 = "Galilei" //Létrehozunk egy változót a "Galilei"-nek
-const ter3 = "Kvantumfizika" //Létrehozunk egy változót a "Kvantumfizika"-nak
-const ido3 = "XIX-XX. század" //Létrehozunk egy változót a "XIX-XX. század"-nak
-const kepv4 = "Max Planck" //Létrehozunk egy változót a "Max Planck"-nek
-const kepv5 = "Albert Einstein" //Létrehozunk egy változót az "Albert Einstein"-nek
-const ter4 = "Modern fizika" //Létrehozunk egy változót a "Modern fizika"-nak
-const ido4 = "XX-XXI. század" //Létrehozunk egy változót a "XX-XXI. század"-nak
-const kepv6 = "Richard Feynman" //Létrehozunk egy változót a "Richard Feynman"-nek
-const kepv7 = "Stephen Hawking" //Létrehozunk egy változót a "Stephen Hawking"-nak
+const fejlec = { //a fejlec objektuma
+    ter: "Fizika területe",  //az objektum ter tulajdonsága
+    ido: "Időszak" ,  //az objektum ido tulajdonsága
+    kepv: "Képviselők"  //az objektum kepv tulajdonsága
+}
+
+const elso = { //az elso sor objektuma
+    ter: "Optika", //az objektum ter tulajdonsága
+    ido: "XI. század" ,  //az objektum ido tulajdonsága
+    kepv: "Alhazen"  //az objektum kepv tulajdonsága
+}
+
+const masodik = { //a masodik sor objektuma
+    ter: "Asztronómia", //az objektum ter tulajdonsága
+    ido: "reneszánsz" , //az objektum ido tulajdonsága
+    kepv: "Kepler", //az objektum kepv tulajdonsága
+    kepv2: "Galilei" //az objektum kepv2 tulajdonsága
+}
+
+const harmadik = { //a harmadik sor objektuma
+    ter: "Kvantumfizika", //az objektum ter tulajdonsága
+    ido: "XIX-XX. század" , //az objektum ido tulajdonsága
+    kepv: "Max Planck", //az objektum kepv tulajdonsága 
+    kepv2: "Albert Einstein" //az objektum kepv2 tulajdonsága
+}
+
+const negyedik = { //a negyedik sor objektuma
+    ter: "Modern fizika", //az objektum ter tulajdonsága
+    ido: "XX-XXI. század" , //az objektum ido tulajdonsága
+    kepv: "Richard Feynman", //az obbjektum kepv tulajdonsága
+    kepv2: "Stephen Hawking" //az objektum kepv2 tulajdonsága
+}
 
 
 const table = document.createElement('table') // table letrehozasa
@@ -28,16 +42,16 @@ const theadr = document.createElement('tr') //tr letrehozasa
 thead.appendChild(theadr) //a tr hozzaadasa a thead hez
 
 const thc1 = document.createElement('th') //laz elso th letrehozasa
-thc1.innerHTML = ter //az elso th szovege ter valtozo
+thc1.innerHTML = fejlec.ter //az elso th szovege a fejlec ter tulajdonsága
 theadr.appendChild(thc1) //a th-t hozzaadjuk a trhez
  
 const thc2 = document.createElement('th') //a haramdik th letrehozasa
-thc2.innerHTML = ido //a masodik th szovege ido valtozo
+thc2.innerHTML = fejlec.ido //a masodik th szovege fejlec ido tulajdonsága
 theadr.appendChild(thc2) //a th-t hozzaadjuk a trhez
 
 const thc3 = document.createElement('th') //a masodik th letrehozasa 
 thc3.colSpan = 2 //beallitjuk a colspant a cellara
-thc3.innerHTML = kepv //a hatmadik th szovege kepv valtozo
+thc3.innerHTML = fejlec.kepv //a hatmadik th szovege dejlec kepv tulajdonsága
 theadr.appendChild(thc3) //a th-t hozzaadjuk a trhez
 
 const tbody = document.createElement('tbody') //tbody letrehozasa
@@ -48,16 +62,16 @@ const tbr1 = document.createElement('tr') //létrehozzuk a tbody 1. sorát
 tbody.appendChild(tbr1) //az 1. sort hozzácsatoljuk a tbody hoz
 
 const r1td1 = document.createElement('td') //létrehozzuk elso cellat a sorban 
-r1td1.innerHTML = ter1 //az elso td szovege ter1 valtozo
+r1td1.innerHTML = elso.ter //az elso td szovege az elso sor objektum ter tulajdonsága
 tbr1.appendChild(r1td1) //hozzáadjuk a cellát az 1. sorhoz
 
 const r1td2 = document.createElement('td') //létrehozzuk a masodik cellat a sorban 
-r1td2.innerHTML = ido1 //az masodik td szovege ido1 valtozo
+r1td2.innerHTML = elso.ido //az masodik td szovege az elso sor objektum ido tulajdonsága
 tbr1.appendChild(r1td2) //hozzáadjuk a cellát az 1. sorhoz
 
 const r1td3 = document.createElement('td') //lletrehozzuk a harmadik cellat a sorban
 r1td3.colSpan = 2 //beallitjuk a colspant a cellara
-r1td3.innerHTML = kepv1 //a harmadik td szovege kepv1 valtozo
+r1td3.innerHTML = elso.kepv //a harmadik td szovege az elso sor objektum kepv tulajdonsága
 tbr1.appendChild(r1td3) //hozzáadjuk a cellát az 1. sorhoz
 
 //2
@@ -65,19 +79,19 @@ const tbr2 = document.createElement('tr') //létrehozzuk a tbody 2. sorát
 tbody.appendChild(tbr2) //a 2. sort hozzácsatoljuk a tbody- hozz
 
 const r2td1 = document.createElement('td') //létrehozzuk elso cellat a sorban 
-r2td1.innerHTML = ter1 //az elso td szovege ter2 valtozo
+r2td1.innerHTML = masodik.ter //az elso td szovege a masodik sor objektumanak ter tulajdonsága
 tbr2.appendChild(r2td1) // hozzácsatoljuk a cellát a 2. sorhoz
 
 const r2td2 = document.createElement('td') //létrehozzuk a masodik cellat a sorban 
-r2td2.innerHTML = ido2 //az masodik td szovege ido2 valtozo
+r2td2.innerHTML = masodik.ido //az masodik td szovege a masodik sor objektumanak ido tulajdonsága
 tbr2.appendChild(r2td2) // hozzácsatoljuk a cellát a 2. sorhoz
 
 const r2td3 = document.createElement('td') //lletrehozzuk a harmadik cellat a sorban
-r2td3.innerHTML = kepv2 //a harmadik td szovege kepv2 valtozo
+r2td3.innerHTML = masodik.kepv //a harmadik td szovege a masodik sor objektumanak kepv tulajdonsága
 tbr2.appendChild(r2td3) // hozzácsatoljuk a cellát a 2. sorhoz
 
 const r2td4 = document.createElement('td') //lletrehozzuk a negyedik cellat a sorban
-r2td4.innerHTML = kepv3 //a ngyedik td szovege kepv3 valtozo
+r2td4.innerHTML = masodik.kepv2 //a ngyedik td szovege a masodik sor objektumanak kepv2 tulajdonsága
 tbr2.appendChild(r2td4) // hozzácsatoljuk a cellát a 2. sorhoz
 
 //3
@@ -85,19 +99,19 @@ const tbr3 = document.createElement('tr') //létrehozzuk a tbody 3. sorát
 tbody.appendChild(tbr3) //az 3. sort hozzácsatoljuk a tbody- hozz
 
 const r3td1 = document.createElement('td') //létrehozzuk az elso cellat a sorban  
-r3td1.innerHTML = ter3 //az elso td szovege ter3 valtozo
+r3td1.innerHTML = harmadik.ter //az elso td szovege a harmadik sor objektumanak ter tulajdonsága
 tbr3.appendChild(r3td1) // hozzácsatoljuk a cellát a 3. sorhoz
 
 const r3td2 = document.createElement('td') //létrehozzuk a masodik cellat a sorban  
-r3td2.innerHTML = ido3 //az masodik td szovege ido3 valtozo
+r3td2.innerHTML = harmadik.ido //az masodik td szovege a harmadik sor objektumanak ido tulajdonsága
 tbr3.appendChild(r3td2) // hozzácsatoljuk a cellát a 3. sorhoz
 
 const r3td3 = document.createElement('td') //létrehozzuk a harmadik cellat a sorban   
-r3td3.innerHTML = kepv4 //a harmadik td szovege kepv4 valtozo
+r3td3.innerHTML = harmadik.kepv //a harmadik td szovege a harmadik sor objektumanak kepv tulajdonsága
 tbr3.appendChild(r3td3) // hozzácsatoljuk a cellát a 3. sorhoz
 
 const r3td4 = document.createElement('td') //létrehozzuk a negyedik cellat a sorban  
-r3td4.innerHTML = kepv5 //a ngyedik td szovege kepv5 valtozo
+r3td4.innerHTML = harmadik.kepv2 //a ngyedik td szovege a harmadik sor objektumanak kepv2 tulajdonsága
 tbr3.appendChild(r3td4) // hozzácsatoljuk a cellát a 3. sorhoz
 
 //4
@@ -105,18 +119,18 @@ const tbr4 = document.createElement('tr') //létrehozzuk a tbody 4. sorát
 tbody.appendChild(tbr4) //a 4. sort hozzácsatoljuk a tbody- hozz
 
 const r4td1 = document.createElement('td') //létrehozzuk az elso cellat a sorban   
-r4td1.innerHTML = ter4 //az elso td szovege ter4 valtozo
+r4td1.innerHTML = negyedik.ter //az elso td szovege a negyedik sor objektumanak ter tulajdonsága
 tbr4.appendChild(r4td1) // hozzácsatoljuk a cellát a 4. sorhoz
 
 const r4td2 = document.createElement('td') //létrehozzuk a masodik cellat a sorban  
-r4td2.innerHTML = ido4 //az masodik td szovege ido4 vaoltozo
+r4td2.innerHTML = negyedik.ido //az masodik td szovege a negyedik sor objektumanak ido tulajdonsága
 tbr4.appendChild(r4td2) // hozzácsatoljuk a cellát a 4. sorhoz
 
 const r4td3 = document.createElement('td') //létrehozzuk a harmadik cellat a sorban  
-r4td3.innerHTML = kepv6 //a harmadik td szovege kepv6 vaoltozo
+r4td3.innerHTML = negyedik.kepv //a harmadik td szovege a negyedik sor objektumanak kepv tulajdonsága
 tbr4.appendChild(r4td3) // hozzácsatoljuk a cellát a 4. sorhoz
 
 const r4td4 = document.createElement('td') //létrehozzuk a negyedik cellat a sorban  
-r4td4.innerHTML = kepv7 //a ngyedik td szovege kepv7 valtozo
+r4td4.innerHTML = negyedik.kepv2 //a ngyedik td szovege a negyedik sor objektumanak kepv2 tulajdonsága
 tbr4.appendChild(r4td4) // hozzácsatoljuk a cellát a 4. sorhoz
 
